20fc5ea5fb48b64df8c3885ba6caecd4d1c633d4
andreas24sfj <andreas24sfj@gmail.com>
2024-12-06 09:40:29 +0100

    comments

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.Win32.SafeHandles;
 
-string filepath = "tekstfil.txt";
+string filepath = "tekstfil.txt"; // lager en variabel som peker til filepathen til tekstfilen min
 
-if (!File.Exists(filepath))
+if (!File.Exists(filepath)) // hvis den fil ikke eksisterer i filepathen:
 {
-    using (StreamWriter streamWriter = File.CreateText(filepath))
+    using (StreamWriter streamWriter = File.CreateText(filepath)) //bruke streamwriter til å lage en tekstfil med filepath prosjekt/tekstfil.txt
     {
-        streamWriter.WriteLine("Dette");
+        streamWriter.WriteLine("Dette");    // skriver det som skal inn i filen.
         streamWriter.WriteLine("er");
         streamWriter.Write("en ");
         streamWriter.WriteLine("tekstfil");
@@ -16,10 +16,10 @@ if (!File.Exists(filepath))
     }
 }
 
-using (StreamReader streamReader = File.OpenText(filepath))
+using (StreamReader streamReader = File.OpenText(filepath)) // bruker StreamReader til å åpne og lese teksten i filen.
 {
-    string? line;
-    while ((line = streamReader.ReadLine()) != null)
+    string? line; // lager en strengvariabel line
+    while ((line = streamReader.ReadLine()) != null) // så lenge det er en tekst på en linje(den leser ikke null), les og skriv ut teksten i console
     {
         Console.WriteLine(line);
     }
